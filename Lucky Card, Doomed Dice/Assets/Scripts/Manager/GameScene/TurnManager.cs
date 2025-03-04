a96c8eb7ae4b74a7cf1b608060deb8c82465ba1d
hej7866 <hej7866@naver.com>
2025-03-04 18:39:22 +0900

    (2025.03.04) 페이즈 시간 동기화(PhotonNetwork.Time) 을 이용한 중앙 동기화i

--- Lucky Card, Doomed Dice/Assets/Scripts/Manager/GameScene/TurnManager.cs
+++ Lucky Card, Doomed Dice/Assets/Scripts/Manager/GameScene/TurnManager.cs
@@ -17,7 +17,7 @@ public class TurnManager : MonoBehaviourPunCallbacks
     public float thinkingTime = 30f;
     public float battleTime = 15f;
     private bool isTurnActive = false;
-    private float turnTimer;
+    private double turnEndTime;
 
     private void Awake()
     {
@@ -43,20 +43,27 @@ public class TurnManager : MonoBehaviourPunCallbacks
             photonView.RPC("SyncTurn", RpcTarget.All, currTurn);
 
             // **① 전략 페이즈 (30초) - 점수 숨기기**
-            turnTimer = thinkingTime; 
-            photonView.RPC("UpdatePhase", RpcTarget.All, "전략 페이즈", turnTimer, false);
-            yield return StartCoroutine(ThinkingPhase());
+            StartTurnTimer(thinkingTime, "전략 페이즈", false);
+            yield return new WaitUntil(() => !isTurnActive);
 
             // **② 전투 페이즈 (15초) - 점수 공개**
-            turnTimer = battleTime;
-            photonView.RPC("UpdatePhase", RpcTarget.All, "전투 페이즈", turnTimer, true);
-            yield return StartCoroutine(BattlePhase());
+            StartTurnTimer(battleTime, "전투 페이즈", true);
+            yield return new WaitUntil(() => !isTurnActive);
 
             currTurn++;
         }
 
         Debug.Log("게임 종료!");
         photonView.RPC("GameOver", RpcTarget.All);
+    }
+
+    private void StartTurnTimer(float duration, string phase, bool showScore)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            turnEndTime = PhotonNetwork.Time + duration;
+            photonView.RPC("SyncTurnTimer", RpcTarget.All, turnEndTime, phase, showScore);
+        }
     }
 
     [PunRPC]
@@ -67,53 +74,31 @@ public class TurnManager : MonoBehaviourPunCallbacks
     }
 
     [PunRPC]
+    private void SyncTurnTimer(double endTime, string phase, bool showScore)
+    {
+        turnEndTime = endTime;
+        isTurnActive = true;
+        Phase.text = phase;
+        UIManager.Instance.ToggleScoreVisibility(showScore);
+    }
+
+    [PunRPC]
     private void GameOver()
     {
         Debug.Log("게임이 끝났습니다! 승패를 결정하세요.");
     }
 
-    private IEnumerator ThinkingPhase()
+    private void Update()
     {
-        Debug.Log("카드 선택 & 주사위 굴리기 단계");
-
-        while (turnTimer > 0)
+        if (isTurnActive)
         {
-            turnTimer -= Time.deltaTime;
-            photonView.RPC("UpdateTurnTimer", RpcTarget.All, turnTimer);
-            yield return null;
+            double timeRemaining = turnEndTime - PhotonNetwork.Time;
+            turnTime.text = Mathf.FloorToInt((float)timeRemaining).ToString();
+            
+            if (timeRemaining <= 0)
+            {
+                isTurnActive = false;
+            }
         }
-
-        Debug.Log("생각하는 시간이 끝났습니다!");
-    }
-
-    private IEnumerator BattlePhase()
-    {
-        Debug.Log("전투 단계");
-
-        while (turnTimer > 0)
-        {
-            turnTimer -= Time.deltaTime;
-            photonView.RPC("UpdateTurnTimer", RpcTarget.All, turnTimer);
-            yield return null;
-        }
-
-        Debug.Log("전투가 끝났습니다!");
-    }
-
-    [PunRPC]
-    private void UpdatePhase(string phase, float time, bool showScore)
-    {
-        Phase.text = phase;
-        turnTimer = time;
-        turnTime.text = Mathf.FloorToInt(turnTimer).ToString();
-
-        UIManager.Instance.ToggleScoreVisibility(showScore);
-    }
-
-    [PunRPC]
-    private void UpdateTurnTimer(float time)
-    {
-        turnTimer = time;
-        turnTime.text = Mathf.FloorToInt(turnTimer).ToString();
     }
 }
